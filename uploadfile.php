5ac2ad6dc88fcbaed76cc8ab9bb51098fb6fc82b
ChrisL_frakkass <dcl.chrisl@18pixel.fr>
2019-01-07 12:00:10 +0100

    upload and mysql connect ok

--- uploadfile.php
+++ uploadfile.php
@@ -9,6 +9,7 @@
         $extensionFichier = $elementsChemin['extension'];
         $extensionsAutorisees = array('jpeg', 'jpg', 'gif', 'png', 'doc', 'pdf', 'bmp', 'txt', 'svg');
         $tailleFichier = $_FILES['file']['size'];
+        $date = date('Y/d/m h:i:s', time());
         
         if (isset($_FILES['file'])) {
             if (!(in_array($extensionFichier, $extensionsAutorisees))) {
@@ -26,84 +27,108 @@
                 if (move_uploaded_file($_FILES['file']['tmp_name'],
                 $repertoireDestination.$nomOrigine)) {
                     echo 'Nous avons sauvegardé votre fichier de '.$tailleFichier.' octets sous le nom '.$nomOrigine;
-                    echo '</br></br> merci '.$_POST['email'].' nous avons envoyé un mail à '.$_POST['mailToSend'];
+                    echo '</br></br> merci '.$_POST['email'].' nous avons envoyé un mail à '.$_POST['mailToSend'].'</br>';
+                    
                     ?>
                     <form></br>
                     <input type="button" value="Retour" onclick="history.go(-1)">
                     </form>
                     <?php
                     // echo 'Le fichier temporaire '.$_FILES['file']['tmp_name'].
-                    // ' a été déplacé vers '.$repertoireDestination.$nomDestination;
+                    // ' a été déplacé vers '.$repertoireDestination.$nomDestination;                      
                     
-                    // $wpdb->insert( 'wp_fichier', array (
-                    //     'idFichier_fichier' => '',
-                    //     'nomFichier_fichier' => $nomOrigine,
-                    //     'dateFichier_fichier' => date('YmdHis'),
-                    //     'tailleFichier_fichier' => $_FILES['mfile']['size']
-                        
-                        
                     // ));
+
+                    //Connexion et envoi des données sur la base de données
                     
                     global $wpdb;
-                    $table = $wpdb-> wp_fichier;
-                    $data = array ('idFichier' => '', 'nomFichier_fichier'=> $nomOrigine, 'dateFichier_fichier' =>'', 'tailleFichier_fichier' => $tailleFichier  );
-                    $wpdb -> insert($table,$data);
-                    var_dump ($data);
-                    
-                    $mail = $_POST['mailToSend'];
-                    $passage_ligne = "\r\n";
-                    $boundary = '-----='.md5(rand());
-                    $subjet = 'Un fichier vous attends!';
-                    
-                    //=====Création du header de l'e-mail
-                    $header = 'From: '.$_POST['mailToSend'].$passage_ligne;
-                    // $header .= 'MIME-Version: 1.0'.$passage_ligne;
-                    $header .= 'Content-type: text/html; charset=UTF-8' .$passage_ligne;
-                    //==========
-                    
-                    $message = 'Vous recevez cet email de la part de '.$_POST['email'].'qui vient de vous envoyer un un fichier. Pour le récupérer suivez ce lien '.$repertoireDestination.$nomOrigine;
-                    
-                    //====Envoi du mail
-                    mail($mail, $subjet, $message, $header);
-                } else {
-                    echo "Le fichier n'a pas été uploadé (trop gros ?) ou ".
-                    'Le déplacement du fichier temporaire a échoué'.
-                    " vérifiez l'existence du répertoire ".$repertoireDestination.'</br>'; 
-                    ?><form>
-                    <input type="button" value="Retour" onclick="history.go(-1)">
-                    </form><?php
+                    $wpdb->insert(
+                        $wpdb->prefix.'fichier',
+                        array(
+                            'idFichier_fichier' => '',
+                            'nomFichier_fichier'=> $nomOrigine,
+                            'dateFichier_fichier' =>$date,
+                            'tailleFichier_fichier' => $tailleFichier
+                        ),
+                        array(
+                            '%s',
+                            '%s',
+                            '%s',
+                            '%d'
+                            )
+                        );
+                        //affiche tous les fichiers postés avec leurs id
+                        $resultats = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}fichier");
+                        foreach ($resultats as $post) {
+                            echo $repertoireDestination.$post->nomFichier_fichier.'</br>'.$post->idFichier_fichier.'</br>';
+                   
+                            echo '</br>';
+                            // echo $Get[]
+                            
+                            // $table = $wpdb-> wp_fichier;
+                            // $data = array ('idFichier' => '', 'nomFichier_fichier'=> $nomOrigine, 'dateFichier_fichier' =>'', 'tailleFichier_fichier' => $tailleFichier  );
+                            // $wpdb -> insert($table,$data);
+                            // var_dump($table);
+                            // var_dump ($nomOrigine);
+                            
+
+                        //Envoi un mail avec les infos 
+                            $mail = $_POST['mailToSend'];
+                            $passage_ligne = "\r\n";
+                            $boundary = '-----='.md5(rand());
+                            $subjet = 'Un fichier vous attends!';
+                            
+                        };
+                        
+                        
+                        //=====Création du header de l'e-mail
+                        $header = 'From: '.$_POST['mailToSend'].$passage_ligne;
+                        // $header .= 'MIME-Version: 1.0'.$passage_ligne;
+                        $header .= 'Content-type: text/html; charset=UTF-8' .$passage_ligne;
+                        //==========
+                        
+                        $message = 'Vous recevez cet email de la part de '.$_POST['email'].'qui vient de vous envoyer un un fichier. Pour le récupérer suivez ce lien '.$repertoireDestination.$nomOrigine;
+                        //====Envoi du mail
+                        mail($mail, $subjet, $message, $header);
+                    } else {
+                        echo "Le fichier n'a pas été uploadé (trop gros ?) ou ".
+                        'Le déplacement du fichier temporaire a échoué'.
+                        " vérifiez l'existence du répertoire ".$repertoireDestination.'</br>'; 
+                        ?><form>
+                        <input type="button" value="Retour" onclick="history.go(-1)">
+                        </form><?php
+                    }
                 }
             }
+            
+            
         }
         
         
-    }
-    
-    
-    
-    if (! (isset($_POST['but_submit']))){        
         
-        // <link rel="stylesheet" href="style.css">
-        
-        ?><form method="POST" action="" enctype="multipart/form-data">
-        <p>Tous les champs sont obligatoires</p>
-        <input type="file" name = "file"/></br>
-        <input type="hidden" name="MAX_FILE_SIZE" value="500000"> <!-- limiter la taille max à 500 ko -->
-        Votre E-mail : <input class="verif-email" type="email" name="email" required/></br>
-        Email destinataire : <input class="verif-email" type="email" name="mailToSend" required/></br>
-        <input type="submit" name="but_submit" value="Envoyer">
-        
-        </form>
-        <?php }
-        // global $wpdb;
-        // $table = $wpdb-> wp_fichier;
-        // $data = array ('idFichier' => '', 'nomFichier_fichier'=> $nomOrigine, 'dateFichier_fichier' => NOW(), 'tailleFichier_fichier' => $_FILES['mfile']['size']  );
-        // $wpdb -> insert($table,$data);
-        
-        
-        
-        // global $wpdb;
-        
-        // $wpdb->query( "SELECT * FROM wp_comments" );
-        
-        // var_dump($wpdb);
+        if (! (isset($_POST['but_submit']))){        
+            
+            // <link rel="stylesheet" href="style.css">
+            
+            ?><form method="POST" action="" enctype="multipart/form-data">
+            <p>Tous les champs sont obligatoires</p>
+            <input type="file" name = "file"/></br>
+            <input type="hidden" name="MAX_FILE_SIZE" value="500000"> <!-- limiter la taille max à 500 ko -->
+            Votre E-mail : <input class="verif-email" type="email" name="email" required/></br>
+            Email destinataire : <input class="verif-email" type="email" name="mailToSend" required/></br>
+            <input type="submit" name="but_submit" value="Envoyer">
+            
+            </form>
+            <?php }
+            // global $wpdb;
+            // $table = $wpdb-> wp_fichier;
+            // $data = array ('idFichier' => '', 'nomFichier_fichier'=> $nomOrigine, 'dateFichier_fichier' => NOW(), 'tailleFichier_fichier' => $_FILES['mfile']['size']  );
+            // $wpdb -> insert($table,$data);
+            
+            
+            
+            // global $wpdb;
+            
+            // $wpdb->query( "SELECT * FROM wp_comments" );
+            
+            // var_dump($wpdb);
